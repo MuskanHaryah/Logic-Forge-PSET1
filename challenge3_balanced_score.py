e02b48919dcc9db2cb4d92ee0e82fa9dedbb59b9
MuskanHaryah <muskanharyah36@gmail.com>
2025-12-31 21:28:06 +0500

    Optimized solution for challenge 3

--- challenge3_balanced_score.py
+++ challenge3_balanced_score.py
@@ -1,24 +1,24 @@
 def balanced_median(scoresA, scoresB):
     m, n = len(scoresA), len(scoresB)
     total = m + n
-    mid = total // 2
+    mid = total// 2
     
     i = j = 0
-    prev = curr = 0
+    prev = cur = 0
     
     for _ in range(mid + 1):
-        prev = curr
+        prev = cur
         
         if i < m and (j >= n or scoresA[i] <= scoresB[j]):
-            curr = scoresA[i]
+            cur = scoresA[i]
             i += 1
         else:
-            curr = scoresB[j]
+            cur = scoresB[j]
             j += 1
     
     if total % 2 == 1:
-        return float(curr)
-    return (prev + curr) / 2.0
+        return float(cur)
+    return (prev + cur) / 2.0
 
 print(balanced_median([1,3], [2]))        
 print(balanced_median([1,2], [3,4]))      
