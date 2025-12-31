55fe22dfd7672b8b78a1972bbacc662ea37d73b6
Muskanharyahh <muskan4606406@cloud.neduet.edu.pk>
2025-12-31 20:51:41 +0500

    Optimized solution using sliding window for challenge X

--- challenge1_team_contribution.py
+++ challenge1_team_contribution.py
@@ -1,9 +1,27 @@
+# def team_contribution(contributions): 
+#     length= len(contributions)
+#     impact=[1]*length 
+#     for i in range(length):
+#         for j in range(length):
+#             if i!=j:
+#                 impact[i]*=contributions[j]
+#     return impact
+# print(team_contribution([1,2,3,4]))  
+# print(team_contribution([-1, 1, 0, -3, 3]))  
+
+
 def team_contribution(contributions): 
     length= len(contributions)
-    impact=[1]*length # here we are doing [1]*length to create an array of 1s of size length
+    prefix=[1]*length
+    suffix=[1]*length
+    impact=[1]*length 
+    for i in range(1,length):
+        prefix[i] = prefix[i-1] * contributions[i-1]
+    for i in range(length-2,-1,-1):
+        suffix[i] = suffix[i+1]* contributions[i+1]
     for i in range(length):
-        for j in range(length):
-            if i!=j:
-                impact[i]*=contributions[j]
+        impact[i] = prefix[i]*suffix[i]
+        
     return impact
 print(team_contribution([1,2,3,4]))  
+print(team_contribution([-1, 1, 0, -3, 3]))  
